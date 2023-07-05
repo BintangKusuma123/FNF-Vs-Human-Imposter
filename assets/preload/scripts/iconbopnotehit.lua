e519fc1e7ff54ed536516b9eaa8f8779d3085722
BintangKusuma123 <95070885+BintangKusuma123@users.noreply.github.com>
2023-07-06 01:50:37 +0700

    Add files via upload

---
+++ assets/preload/scripts/iconbopnotehit.lua
@@ -0,0 +1,6 @@
+function goodNoteHit(id, direction, noteType, isSustainNote)
+         scaleObject('iconP1',1.25,1.2)
+end
+function opponentNoteHit(id, direction, note_type, sustained)
+         scaleObject('iconP2',1.25,1.2)
+end
